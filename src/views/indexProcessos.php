eebfd721c8da6d87d8c362cb28fa885443a4768c
Kyllder Rocha <kyllder.rocha@gmail.com>
2022-05-20 18:03:33 -0300

    Finalizando base dos filtros

---
+++ src/views/indexProcessos.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Processos</title>
+</head>
+    <body class='bg-[#96D5F3]'>
+        
+    <?php
+        include('menu.php')
+    ?>
+        <!-- Processo Index -->
+        <div class="w-screen absolute">
+            <!--Filtro -->
+            <div class="bg-[#FFFFFF] mx-16 my-5 relative rounded-md px-14 py-4">
+                <form class="grid grid-cols-4 gap-x-5 gap-y-3" action="#" method="POST">
+                    <div class="flex items-center">
+                        <label for="email-address" class="sr-only">Email address</label>
+                        <input id="email-address" name="email" type="email" autocomplete="email" required 
+                        class="grow appearance-none relative block px-3 py-2 border border-gray-300 placeholder-gray-500
+                         text-gray-900 rounded-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm" 
+                         placeholder="Email address">
+                    </div>
+                    <div class="flex items-center">
+                        <label for="email-address" class="sr-only">Email address</label>
+                        <input id="email-address" name="email" type="email" autocomplete="email" required 
+                        class="grow appearance-none relative block px-3 py-2 border border-gray-300 placeholder-gray-500
+                         text-gray-900 rounded-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm" 
+                         placeholder="Email address">
+                    </div>
+                    <div class="flex items-center">
+                        <label for="email-address" class="sr-only">Email address</label>
+                        <input id="email-address" name="email" type="email" autocomplete="email" required 
+                        class="grow appearance-none relative block px-3 py-2 border border-gray-300 placeholder-gray-500
+                         text-gray-900 rounded-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm" 
+                         placeholder="Email address">
+                    </div>
+                    <div class="flex items-center">
+                        <label for="email-address" class="sr-only">Email address</label>
+                        <input id="email-address" name="email" type="email" autocomplete="email" required 
+                        class="grow appearance-none relative block px-3 py-2 border border-gray-300 placeholder-gray-500
+                         text-gray-900 rounded-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm" 
+                         placeholder="Email address">
+                    </div>
+                    <div class="flex items-center">
+                        <label for="email-address" class="sr-only">Email address</label>
+                        <input id="email-address" name="email" type="email" autocomplete="email" required 
+                        class="grow appearance-none relative block px-3 py-2 border border-gray-300 placeholder-gray-500
+                         text-gray-900 rounded-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm" 
+                         placeholder="Email address">
+                    </div>
+                    <div class="flex items-center">
+                        <label for="email-address" class="sr-only">Email address</label>
+                        <input id="email-address" name="email" type="email" autocomplete="email" required 
+                        class="grow appearance-none relative block px-3 py-2 border border-gray-300 placeholder-gray-500
+                         text-gray-900 rounded-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm" 
+                         placeholder="Email address">
+                    </div>
+                    <div class="flex items-center">
+                        <label for="email-address" class="sr-only">Email address</label>
+                        <input id="email-address" name="email" type="email" autocomplete="email" required 
+                        class="grow appearance-none relative block px-3 py-2 border border-gray-300 placeholder-gray-500
+                         text-gray-900 rounded-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm" 
+                         placeholder="Email address">
+                    </div>
+                    <div class="flex items-center">
+                        <label for="email-address" class="sr-only">Email address</label>
+                        <input id="email-address" name="email" type="email" autocomplete="email" required 
+                        class="grow appearance-none relative block px-3 py-2 border border-gray-300 placeholder-gray-500
+                         text-gray-900 rounded-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm" 
+                         placeholder="Email address">
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+    </body>
+</html>
---
+++ src/views/menu.php
@@ -0,0 +1,16 @@
+
+<script src="https://cdn.tailwindcss.com"></script>
+
+<div class="bg-[#FFFFFF] text-zinc-700 flex px-[3.75rem] space-x-12 justify-items-center items-center h-14 border-solid border-2 border-sky-500">
+    <div class="Logo">
+        <img src="https://advocaciatrento.adv.br/wp-content/uploads/2020/12/LOGO.png" alt="" width="40" height="40">
+    </div>
+        <ul class="grow flex space-x-6">
+            <li>Processos</li>
+            <li>Clientes</li>
+            <li>Usúarios</li>
+        </ul>
+    <div class="User">
+        <img src="https://e7.pngegg.com/pngimages/304/275/png-clipart-user-profile-computer-icons-profile-miscellaneous-logo-thumbnail.png" alt="" width="40" height="40">
+    </div>
+</div>
